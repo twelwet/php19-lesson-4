a6c00f4e20eb5f4b88ef35f6f744d1fcd28ffb88
Maxim Akimov <akimovmax@mail.ru>
2018-01-11 17:39:44 +0300

    Добавил функцию кеширования

--- weather.php
+++ weather.php
@@ -1,5 +1,31 @@
 <?php
-    $url = 'http://api.openweathermap.org/data/2.5/weather?q=Moscow&mode=html&APPID=3858b299475c554824bb6d348e24a966';
-    $json = file_get_contents($url);
-    echo $json;
-?>
+
+    $city = 'Moscow';
+    $appid = '3858b299475c554824bb6d348e24a966';
+    $mode = 'html';
+    $url = 'http://api.openweathermap.org/data/2.5/weather?q=' . $city . '&mode=' . $mode . '&APPID=' . $appid;
+    $cacheFile = 'cache.txt';
+
+    $showCache = function($filename) {
+        echo file_get_contents($filename);
+    };
+
+    $updateCache = function($link, $filename) {
+        file_put_contents($filename, file_get_contents($link));
+    };
+
+    switch(file_exists($cacheFile)) {
+        case true:
+            $lastCacheChange = (time(void) - filectime($cacheFile));
+            if ($lastCacheChange <= 3600) {
+                $showCache($cacheFile);
+            } else {
+                $updateCache($url, $cacheFile);
+                $showCache($cacheFile);
+            }
+            break;
+        case false:
+            $updateCache($url, $cacheFile);
+            $showCache($cacheFile);
+            break;
+    }
