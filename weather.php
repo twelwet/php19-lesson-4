dded324cd37c87fdbf376f3e0e7697d2c83f7d50
Maxim Akimov <akimovmax@mail.ru>
2018-01-20 02:29:01 +0300

    Внес правки согласно замечаниям

--- weather.php
+++ weather.php
@@ -1,31 +1,18 @@
 <?php
 
-    $city = 'Moscow';
+    $city = 'Deli';
     $appid = '3858b299475c554824bb6d348e24a966';
     $mode = 'html';
     $url = 'http://api.openweathermap.org/data/2.5/weather?q=' . $city . '&mode=' . $mode . '&APPID=' . $appid;
     $cacheFile = 'cache.txt';
 
-    $showCache = function($filename) {
-        echo file_get_contents($filename);
-    };
-
-    $updateCache = function($link, $filename) {
-        file_put_contents($filename, file_get_contents($link));
-    };
-
-    switch(file_exists($cacheFile)) {
-        case true:
-            $lastCacheChange = (time(void) - filectime($cacheFile));
-            if ($lastCacheChange <= 3600) {
-                $showCache($cacheFile);
-            } else {
-                $updateCache($url, $cacheFile);
-                $showCache($cacheFile);
-            }
-            break;
-        case false:
-            $updateCache($url, $cacheFile);
-            $showCache($cacheFile);
-            break;
+    if (file_exists($cacheFile)) {
+        $lastCacheChange = (time(void) - filectime($cacheFile));
+        if ($lastCacheChange > 3600) {
+            file_put_contents($cacheFile, file_get_contents($url));
+        }
+        echo file_get_contents($cacheFile);
+    } else {
+        file_put_contents($cacheFile, file_get_contents($url));
+        echo file_get_contents($cacheFile);
     }
