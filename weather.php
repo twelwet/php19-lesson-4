da44f82fc1903cc7e9f0391d84f4d6bf8d619edd
Maxim Akimov <akimovmax@mail.ru>
2018-01-23 15:52:36 +0300

    Упростил условие подгрузки кэша

--- weather.php
+++ weather.php
@@ -1,16 +1,12 @@
 <?php
 
-    $city = 'Deli';
+    $city = 'Saigon';
     $appid = '3858b299475c554824bb6d348e24a966';
     $mode = 'html';
     $url = 'http://api.openweathermap.org/data/2.5/weather?q=' . $city . '&mode=' . $mode . '&APPID=' . $appid;
     $cacheFile = 'cache.txt';
 
-    if (file_exists($cacheFile)) {
-        $lastCacheChange = (time(void) - filectime($cacheFile));
-        if ($lastCacheChange > 3600) {
-            file_put_contents($cacheFile, file_get_contents($url));
-        }
+    if (file_exists($cacheFile) && ((time() - filectime($cacheFile)) <= 10)) {
         echo file_get_contents($cacheFile);
     } else {
         file_put_contents($cacheFile, file_get_contents($url));
